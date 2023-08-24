5351d401be6169adb69880fa3159b0fefe607111
matias <matias36@live.cl>
2023-08-24 18:16:36 -0400

    Create taskSlice

---
+++ web_client/src/features/tasks/redux/tasksSlice.ts
@@ -0,0 +1,71 @@
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {TaskState} from "../../../interfaces/taskState";
+
+const initialState: TaskState = {
+    formData: {
+        description: '',
+        dueDate: '',
+    },
+    tasksSelected: [],
+    ordenOption: {
+        orderBy: 'createdAt', // Order initial
+        order: 'desc',
+    },
+    filterOption: {
+        status: "All",
+        dueDate: {
+            from: null,
+            to: null,
+        },
+        createdAt: null,
+        description: "",
+    }
+}
+
+export const taskSlice = createSlice({
+    name: 'task',
+    initialState,
+    reducers: {
+        setFormData: (state, action) => {
+            state.formData = action.payload;
+        },
+        setTasksSelected: (state, action) => {
+            const {taskId, checked} = action.payload;
+            if (checked) {
+                state.tasksSelected.push(taskId);
+            } else {
+                state.tasksSelected = state.tasksSelected.filter((item) => item !== taskId);
+            }
+        },
+        clearTasksSelected: (state) => {
+            state.tasksSelected = [];
+        },
+        setOrdenOption: (state, action) => {
+            state.ordenOption = action.payload;
+        },
+        setStatusOption: (state, action) => {
+            state.filterOption.status = action.payload;
+        },
+        setDescriptionOption: (state, action) => {
+            state.filterOption.description = action.payload;
+        },
+        setDueDateOption: (state, action) => {
+            state.filterOption.dueDate = action.payload;
+        }
+
+    }
+})
+
+export const taskReducer = taskSlice.reducer;
+
+export const {
+    setFormData,
+    setTasksSelected,
+    clearTasksSelected,
+    setOrdenOption,
+    setStatusOption,
+    setDescriptionOption,
+    setDueDateOption
+} = taskSlice.actions;
+
+
