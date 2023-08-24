9e43e1e0bb29bf744d57784fdf2b8ec51baedbf6
matias <matias36@live.cl>
2023-08-24 18:18:07 -0400

    Add Tasks component

--- web_client/src/App.tsx
+++ web_client/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Tasks from "./features/tasks/Tasks";
 
 function App() {
   return (
     <div>
-      <h1>Tasks</h1>
+        <Tasks/>
     </div>
   );
 }
