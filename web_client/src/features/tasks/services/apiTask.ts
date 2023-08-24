c5d7bdacc91152e5018517040d9fd7983bdb2150
matias <matias36@live.cl>
2023-08-24 18:09:39 -0400

    Creta api task with rtk query

---
+++ web_client/src/features/tasks/services/apiTask.ts
@@ -0,0 +1,31 @@
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+
+type Task = {
+    id: number
+    description: string
+    createdAt: string
+    dueDate: string
+    isComplete: boolean
+}
+const baseUrl = 'http://localhost:3001';
+export const taskApi = createApi({
+    reducerPath: 'taskApi',
+    baseQuery: fetchBaseQuery({baseUrl}),
+    tagTypes: ['Task'],
+    endpoints: (builder) => ({
+        getTasks: builder.query({
+            query: () => '/tasks',
+            providesTags: ['Task'],
+        }),
+        updateTask: builder.mutation({
+            query: ({id, ...patch}) => ({
+                url: `/tasks/${id}`,
+                method: 'PATCH',
+                body: patch,
+            }),
+            invalidatesTags: ['Task'],
+        }),
+    })
+})
+
+export const {useGetTasksQuery, useUpdateTaskMutation} = taskApi;
