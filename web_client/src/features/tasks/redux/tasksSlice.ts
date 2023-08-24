b76e733711d5b6a48f11045d143e30455dd58409
matias <matias36@live.cl>
2023-08-24 18:17:24 -0400

    Remuve PayloadAction

--- web_client/src/features/tasks/redux/tasksSlice.ts
+++ web_client/src/features/tasks/redux/tasksSlice.ts
@@ -1,4 +1,4 @@
-import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {createSlice} from "@reduxjs/toolkit";
 import {TaskState} from "../../../interfaces/taskState";
 
 const initialState: TaskState = {
