4afaf3bcb043680374d55e270314975f5b7d1073
matias <matias36@live.cl>
2023-08-25 00:24:00 -0400

    Add type prop

--- web_client/src/components/buttonProgress/ButtonProgress.tsx
+++ web_client/src/components/buttonProgress/ButtonProgress.tsx
@@ -4,14 +4,15 @@ import CheckCircleIcon from "../icons/CheckCircleIcon";
 
 interface Props {
     text: string
-    onClick: () => void
-    loading: boolean,
+    onClick?: () => void
+    loading: boolean | undefined,
     disabled?: boolean
+    type?: "button" | "submit" | "reset" | undefined
 }
 
-const ButtonProgress: React.FC<Props> = ({text, onClick, loading, disabled}) => {
+const ButtonProgress: React.FC<Props> = ({text, onClick, loading, disabled, type}) => {
     return (
-        <Button variant="contained" onClick={onClick} disabled={loading || disabled} sx={{display: "flex", gap: 1, height: "32px"}}>
+        <Button type={type} variant="contained" onClick={onClick} disabled={loading || disabled} sx={{display: "flex", gap: 1, height: "32px"}}>
             {text} {loading ? <CircularProgress size={24} sx={{color: "#ffffff"}}/> :
             <CheckCircleIcon width={24} height={24} color={"#ffffff"}/>}
         </Button>
